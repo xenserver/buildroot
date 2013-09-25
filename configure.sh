0a31e6717b09686553dbe98348ab13d16650823f
Euan Harris <euan.harris@citrix.com>
2013-09-25 16:52:33 +0100

    configure: Merge RPM setup back in
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- configure.sh
+++ configure.sh
@@ -1,30 +1,54 @@
-#!/bin/sh
+#!/bin/bash
 
-ARCH=amd64
-DIST=raring
-BASETGZ=/var/cache/pbuilder/base-$DIST-$ARCH.tgz
+if [ `lsb_release -si` == "Fedora" ] ; then
 
-apt-get install pbuilder python-rpm curl
-mkdir -p BUILD
+	echo "Configuring RPM-based build"
 
-echo -n "Writing pbuilder configuration..."
-mkdir -p pbuilder
-sed -e "s|@PWD@|$PWD|g" -e "s|@ARCH@|$ARCH|g" -e "s|@BASETGZ@|$BASETGZ|g" -e "s|@DIST@|$DIST|g" pbuilderrc.in > pbuilder/pbuilderrc-$DIST-$ARCH
-sed -e "s|@PWD@|$PWD|g" D05deps.in > pbuilder/D05deps
-chmod 755 pbuilder/D05deps
-echo " done"
+	sudo yum install mock rpm-build
 
-if [ -f $BASETGZ ] ; then
-    echo $BASETGZ exists - updating
-    sudo pbuilder --update --override-config --configfile $PWD/pbuilder/pbuilderrc-$DIST-$ARCH
-else
-    echo $BASETGZ does not exist - creating
-    sudo pbuilder --create --configfile $PWD/pbuilder/pbuilderrc-$DIST-$ARCH
+	echo -n "Writing mock configuration..."
+	mkdir -p mock
+	sed "s|@PWD@|$PWD|" xenserver.cfg.in > mock/xenserver.cfg
+	ln -fs /etc/mock/default.cfg mock/
+	ln -fs /etc/mock/site-defaults.cfg mock/
+	ln -fs /etc/mock/logging.ini mock/
+	echo " done"
+
+	echo -n "Initializing repository..."
+	mkdir -p RPMS
+	createrepo --quiet RPMS
+	echo " done"
+
+elif [ `lsb_release -si` == "Ubuntu" ] ; then
+
+	echo "Configuring DEB-based build"
+
+	ARCH=amd64
+	DIST=raring
+	BASETGZ=/var/cache/pbuilder/base-$DIST-$ARCH.tgz
+
+	sudo apt-get install pbuilder python-rpm curl
+	mkdir -p BUILD
+
+	echo -n "Writing pbuilder configuration..."
+	mkdir -p pbuilder
+	sed -e "s|@PWD@|$PWD|g" -e "s|@ARCH@|$ARCH|g" -e "s|@BASETGZ@|$BASETGZ|g" -e "s|@DIST@|$DIST|g" pbuilderrc.in > pbuilder/pbuilderrc-$DIST-$ARCH
+	sed -e "s|@PWD@|$PWD|g" D05deps.in > pbuilder/D05deps
+	chmod 755 pbuilder/D05deps
+	echo " done"
+
+	if [ -f $BASETGZ ] ; then
+	    echo $BASETGZ exists - updating
+	    sudo pbuilder --update --override-config --configfile $PWD/pbuilder/pbuilderrc-$DIST-$ARCH
+	else
+	    echo $BASETGZ does not exist - creating
+	    sudo pbuilder --create --configfile $PWD/pbuilder/pbuilderrc-$DIST-$ARCH
+	fi
+
+	echo -n "Initializing repository..."
+	mkdir -p RPMS SRPMS
+	(cd RPMS; apt-ftparchive packages . > Packages)
+	(cd SRPMS; apt-ftparchive sources . > Sources)
+	echo " done"
+
 fi
-
-echo -n "Initializing repository..."
-mkdir -p RPMS SRPMS
-(cd RPMS; apt-ftparchive packages . > Packages)
-(cd SRPMS; apt-ftparchive sources . > Sources)
-echo " done"
-
