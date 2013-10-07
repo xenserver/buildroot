5008a6ee01e16ebfcf933e55b982bd184fe8d77e
Mate Lakat <mate.lakat@citrix.com>
2013-10-07 15:21:10 +0100

    Inject key for Anil Madhavapeddy

--- configure.sh
+++ configure.sh
@@ -66,6 +66,22 @@ KQ3afU1hlF6EsITRd5qGry7ftxoLKOrVp8qSw9O/PdFgBTTGvgE=
 =ZOqF
 -----END PGP PUBLIC KEY BLOCK-----
 KEYFILE
+            # inject Keyfile for Launchpad PPA for Anil Madhavapeddy
+            sudo pbuilder --execute --configfile $PWD/pbuilder/pbuilderrc-$DIST-$ARCH --save-after-exec -- /usr/bin/apt-key add - << KEYFILE
+-----BEGIN PGP PUBLIC KEY BLOCK-----
+Version: SKS 1.1.4
+Comment: Hostname: keyserver.ubuntu.com
+
+mI0EUd0+wQEEAMOeX/cGrgub9dEn9xjViAQub7w94JhGYKEpS2w79N3nQKA5NLBxpwvwH9xb
+R7tjVJ11g/feZ+xKkbtcDNHc+BW8IpKf4x7qPy1JrDZ0c8KQhGA6TJY61Wgg4Rwzzi9l1n7L
+G+EkIPotbHY0D27zqzFqwIKj+SbJFPZ9Ty1Z2VCLABEBAAG0I0xhdW5jaHBhZCBQUEEgZm9y
+IEFuaWwgTWFkaGF2YXBlZGR5iLgEEwECACIFAlHdPsECGwMGCwkIBwMCBhUIAgkKCwQWAgMB
+Ah4BAheAAAoJEFstDFVhcHsJryAD/01EFC2zz8LjmEvJX3hPBkkDipD+NCPsuxe78H1QR7AV
+MLzqar1aYcQk/zVUGJDdIcbJnHJ2OnyeeJ0wOVOmJKSrch+jePUkLzM3bWdx5fbK5b4o2nYW
+Xp9mmv8bABlmMr5PDd6/G9y5HTBeImUe7v6a4EtFnF3LnBY0Nkmh9QGq
+=4ul6
+-----END PGP PUBLIC KEY BLOCK-----
+KEYFILE
 
 	fi
 
