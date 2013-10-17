c87d8ad9e4386c3aecba0ada1ed600188f9c4832
Euan Harris <euan.harris@citrix.com>
2013-10-17 16:07:40 +0100

    scripts: Move RPM-specific scripts to scripts/rpm
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- scripts/rpm/configure.sh
+++ scripts/rpm/configure.sh
@@ -7,7 +7,7 @@ rpm -q mock rpm-build >/dev/null 2>&1 || sudo yum install -y mock rpm-build
 
 echo -n "Writing mock configuration..."
 mkdir -p mock
-sed "s|@PWD@|$PWD|" xenserver.cfg.in > mock/xenserver.cfg
+sed "s|@PWD@|$PWD|" scripts/rpm/xenserver.cfg.in > mock/xenserver.cfg
 ln -fs /etc/mock/default.cfg mock/
 ln -fs /etc/mock/site-defaults.cfg mock/
 ln -fs /etc/mock/logging.ini mock/
