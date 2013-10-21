8d912ad1aa601f3ab4737398b6823c041200867d
Euan Harris <euan.harris@citrix.com>
2013-10-21 15:02:35 +0000

    debian: Install pinning preference file correctly, and force-yes in install
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- scripts/deb/install.sh
+++ scripts/deb/install.sh
@@ -8,9 +8,9 @@ sed -e "s,@PWD@,$PWD,g" scripts/deb/xapi.list.in > scripts/deb/xapi.list
 install -m 0644 scripts/deb/xapi.list /etc/apt/sources.list.d/xapi.list
 
 # Configure apt to prefer packages from the local repository
-install -m 0644 scripts/deb/xapi.preference /etc/apt/preferences.d/xapi
+install -m 0644 scripts/deb/xapi.pref /etc/apt/preferences.d/xapi
 
 # Install
 apt-get update
-apt-get install -y xenserver-core
+apt-get install -y --force-yes xenserver-core
 
