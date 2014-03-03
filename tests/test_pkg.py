bd7ef6135fdfbcac44d55973b28ecf9f0a395c2a
Euan Harris <euan.harris@citrix.com>
2014-03-03 15:45:49 +0000

    test_pkg: Use unittest's assertEqual() instead of plain assert
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- tests/test_pkg.py
+++ tests/test_pkg.py
@@ -8,54 +8,59 @@ import pkg
 
 class RpmTests(unittest.TestCase):
     def setUp(self):
+        # 'setUp' breaks Pylint's naming rules
+        # pylint: disable=C0103
         self.spec = pkg.Spec("tests/data/ocaml-cohttp.spec", dist=".el6")
 
     def test_good_filename_preprocessor(self):
         pkg.Spec("tests/data/ocaml-cohttp.spec.in")
 
     def test_bad_filename(self):
-        self.assertRaises(pkg.SpecNameMismatch, pkg.Spec, "tests/data/bad-name.spec")
+        self.assertRaises(pkg.SpecNameMismatch, pkg.Spec,
+                          "tests/data/bad-name.spec")
 
     def test_bad_filename_preprocessor(self):
-        self.assertRaises(pkg.SpecNameMismatch, pkg.Spec, "tests/data/bad-name.spec.in")
+        self.assertRaises(pkg.SpecNameMismatch, pkg.Spec,
+                          "tests/data/bad-name.spec.in")
 
     def test_name(self):
-        assert self.spec.name() == "ocaml-cohttp"
+        self.assertEqual(self.spec.name(), "ocaml-cohttp")
 
     def test_specpath(self):
-        assert self.spec.specpath() == "./SPECS/ocaml-cohttp.spec"
+        self.assertEqual(self.spec.specpath(), "./SPECS/ocaml-cohttp.spec")
 
     def test_version(self):
-        assert self.spec.version() == "0.9.8"
+        self.assertEqual(self.spec.version(), "0.9.8")
 
     def test_provides(self):
-        assert self.spec.provides() == \
-            set(["ocaml-cohttp", "ocaml-cohttp-devel"])
+        self.assertEqual(self.spec.provides(),
+            set(["ocaml-cohttp", "ocaml-cohttp-devel"]))
 
     def test_source_urls(self):
-        assert sorted(self.spec.source_urls()) == \
-            sorted(["https://github.com/mirage/ocaml-cohttp/archive/ocaml-cohttp-0.9.8/ocaml-cohttp-0.9.8.tar.gz"])
+        self.assertEqual(self.spec.source_urls(),
+            ["https://github.com/mirage/ocaml-cohttp/archive/"
+                 "ocaml-cohttp-0.9.8/ocaml-cohttp-0.9.8.tar.gz"])
 
     def test_source_paths(self):
-        assert sorted(self.spec.source_paths()) == \
-            sorted(["./SOURCES/ocaml-cohttp-0.9.8.tar.gz"])
+        self.assertEqual(self.spec.source_paths(),
+            ["./SOURCES/ocaml-cohttp-0.9.8.tar.gz"])
 
     def test_buildrequires(self):
-        assert self.spec.buildrequires() == \
+        self.assertEqual(self.spec.buildrequires(),
             set(["ocaml", "ocaml-findlib", "ocaml-re-devel",
                  "ocaml-uri-devel", "ocaml-cstruct-devel",
                  "ocaml-lwt-devel", "ocaml-ounit-devel",
                  "ocaml-ocamldoc", "ocaml-camlp4-devel",
-                 "openssl", "openssl-devel"])
+                 "openssl", "openssl-devel"]))
 
     def test_source_package_path(self):
-        assert self.spec.source_package_path() == \
-            "./SRPMS/ocaml-cohttp-0.9.8-1.el6.src.rpm"
+        self.assertEqual(self.spec.source_package_path(),
+            "./SRPMS/ocaml-cohttp-0.9.8-1.el6.src.rpm")
 
     def test_binary_package_paths(self):
-        assert sorted(self.spec.binary_package_paths()) == \
+        self.assertEqual(sorted(self.spec.binary_package_paths()),
             sorted(["./RPMS/x86_64/ocaml-cohttp-0.9.8-1.el6.x86_64.rpm",
-             "./RPMS/x86_64/ocaml-cohttp-devel-0.9.8-1.el6.x86_64.rpm"])
+             "./RPMS/x86_64/ocaml-cohttp-devel-0.9.8-1.el6.x86_64.rpm"]))
 
 
 class DebTests(unittest.TestCase):
@@ -80,41 +85,42 @@ class DebTests(unittest.TestCase):
                              map_name=map_rpm_to_deb)
 
     def test_name(self):
-        assert self.spec.name() == "ocaml-cohttp"
+        self.assertEqual(self.spec.name(), "ocaml-cohttp")
 
     def test_specpath(self):
-        assert self.spec.specpath() == "./SPECS/ocaml-cohttp.spec"
+        self.assertEqual(self.spec.specpath(), "./SPECS/ocaml-cohttp.spec")
 
     def test_version(self):
-        assert self.spec.version() == "0.9.8"
+        self.assertEqual(self.spec.version(), "0.9.8")
 
     def test_provides(self):
-        assert self.spec.provides() == \
-            set(["libcohttp-ocaml", "libcohttp-ocaml-dev"])
+        self.assertEqual(self.spec.provides(),
+            set(["libcohttp-ocaml", "libcohttp-ocaml-dev"]))
 
     def test_source_urls(self):
-        assert sorted(self.spec.source_urls()) == \
-            sorted(["https://github.com/mirage/ocaml-cohttp/archive/ocaml-cohttp-0.9.8/ocaml-cohttp-0.9.8.tar.gz"])
+        self.assertEqual(self.spec.source_urls(),
+            ["https://github.com/mirage/ocaml-cohttp/archive/"
+                 "ocaml-cohttp-0.9.8/ocaml-cohttp-0.9.8.tar.gz"])
 
     def test_source_paths(self):
-        assert sorted(self.spec.source_paths()) == \
-            sorted(["./SOURCES/ocaml-cohttp-0.9.8.tar.gz"])
+        self.assertEqual(self.spec.source_paths(),
+            ["./SOURCES/ocaml-cohttp-0.9.8.tar.gz"])
 
     def test_buildrequires(self):
-        assert self.spec.buildrequires() == \
+        self.assertEqual(self.spec.buildrequires(),
             set(["ocaml-nox", "ocaml-native-compilers",
                  "ocaml-findlib", "libre-ocaml-dev",
                  "liburi-ocaml-dev", "libcstruct-ocaml-dev",
                  "liblwt-ocaml-dev", "libounit-ocaml-dev",
                  "camlp4", "camlp4-extra", "libssl1.0.0",
-                 "libssl-dev"])
+                 "libssl-dev"]))
 
     def test_source_package_path(self):
-        assert self.spec.source_package_path() == \
-            "./SRPMS/libcohttp-ocaml_0.9.8-1.dsc"
+        self.assertEqual(self.spec.source_package_path(),
+            "./SRPMS/libcohttp-ocaml_0.9.8-1.dsc")
 
     def test_binary_package_paths(self):
-        assert sorted(self.spec.binary_package_paths()) == \
+        self.assertEqual(sorted(self.spec.binary_package_paths()),
             sorted(["./RPMS/libcohttp-ocaml_0.9.8-1_amd64.deb",
-             "./RPMS/libcohttp-ocaml-dev_0.9.8-1_amd64.deb"])
+             "./RPMS/libcohttp-ocaml-dev_0.9.8-1_amd64.deb"]))
 
