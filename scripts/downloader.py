b31d5a5205684fe7b2767f08813786315522caaa
David Scott <dave.scott@citrix.com>
2014-06-07 14:08:48 +0000

    Add downloader script
    * detects non-archives and deletes them (e.g. HTML error pages etc)
    * retries failed downloads several times
    * canonicalise github.com URLs
    
    The github.com URLs we're using are of the form
    
      https://github.com/org/project/archive/tag/filename-we-want.ext
    
    unfortunately these URLs don't always work, so we rewrite them to
    the standard URL of the form
    
      https://github.com/org/project/archive/tag.ext
    
    Signed-off-by: David Scott <dave.scott@citrix.com>

---
+++ scripts/downloader.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import sys
+import urlparse
+import os
+import subprocess
+import time
+
+def guess_file_type(path):
+	line = subprocess.check_output(["file", path])
+	# file: description
+	if len(line) < (len(path) + 2):
+		print >>sys.stderr, "Malformed output from 'file %s': '%s'" % (path, line)
+		exit(1)
+	return line[len(path) + 2:].strip()
+
+def looks_like_an_archive(path):
+	ty = guess_file_type(path)
+	if ty.startswith("ASCII") or ty.startswith("HTML"):
+		return False
+	if ty.startswith("gzip") or ty.startswith("bzip"):
+		return True
+	print >>sys.stderr, "%s has an unrecognised file type: %s" % (path, ty)
+	print >>sys.stderr, "Please extend %s:looks_like_an_archive to include this case."
+	exit(1)
+
+def download(url, destination):
+	args = ["curl", "--silent", "--show-error", "-L", "-o", destination, url]
+	print >>sys.stderr, "Running %s" % (" ".join(args))
+	returncode = subprocess.call(args)
+	if returncode <> 0:
+		print >>sys.stderr, "Downloading %s failed: sleeping 5s" % url
+		time.sleep(5)
+
+def look_for_it(url, destination):
+	for attempt in [5, 4, 3, 2, 1, 0]:
+		if os.path.exists(destination):
+			if looks_like_an_archive(destination):
+				print >>sys.stderr, "%s exists and looks like an archive" % destination
+				return
+			else:
+				print >>sys.stderr, "%s is not an archive, deleting it" % destination
+				os.unlink(destination)
+				if attempt == 0:
+					print >>sys.stderr, "That was our last attempt so giving up."
+					exit(1)
+		else:
+			print >>sys.stderr, "%s does not exist: downloading" % destination
+			download(url, destination)
+
+if __name__ == "__main__":
+	if len(sys.argv) <> 3:
+		print >>sys.stderr, "Wrong number of arguments. Use %s <url> <destination>" % sys.argv[0]
+		exit(1)
+	url_string = sys.argv[1]
+	destination = sys.argv[2]
+	url = urlparse.urlparse(url_string)
+	path = url.path.split('/')
+	# Avoid relying on github to set the destination filename
+	if url.netloc == "github.com" and path[-3] == "archive":
+		ext = None
+		possible_exts = [ ".tar", ".tar.gz", ".zip", ".tbz", "tar.bz2" ]
+		for e in possible_exts:
+			if url.path.endswith(e):
+				ext = e
+				break
+		if not ext:
+			print >>sys.stderr, "I did not recognise extension of %s. I know about: %s" % (url.path, ", ".join(possible_exts))
+			exit(1)
+		url_path = "/".join(path[0:-2] + [ path[-2] + ext ])
+		url_string = str(urlparse.urlunsplit((url.scheme, url.netloc, url_path, url.query, url.fragment),))
+	look_for_it(url_string, destination)
