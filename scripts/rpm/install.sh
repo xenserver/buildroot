e09c496e96328acca13fb99e0562a96fe48abfad
David Scott <dave.scott@citrix.com>
2014-08-27 13:49:15 +0000

    Incorporate jonludlam's cleanups
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- scripts/rpm/install.sh
+++ scripts/rpm/install.sh
@@ -11,16 +11,11 @@ sed \
     scripts/rpm/xapi.repo.in > scripts/rpm/xapi.repo
 install -m 0644 scripts/rpm/xapi.repo /etc/yum.repos.d/xapi.repo
 
-#install -m 0644 scripts/rpm/xen-c6.repo /etc/yum.repos.d/xen-c6.repo
-#install -m 0644 scripts/rpm/xen-c6-RC1.repo /etc/yum.repos.d/xen-c6-RC1.repo
 install -m 0644 scripts/rpm/centos-xen-4-4.repo /etc/yum.repos.d/centos-xen-4-4.repo
 
 install -m 0644 scripts/rpm/epel.repo /etc/yum.repos.d/epel.repo
-install -m 0644 scripts/rpm/epel-testing.repo /etc/yum.repos.d/epel-testing.repo
-install -m 0644 scripts/rpm/remi.repo /etc/yum.repos.d/remi.repo
 
 install -m 0644 scripts/rpm/RPM-GPG-KEY-EPEL-6 /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6
-install -m 0644 scripts/rpm/RPM-GPG-KEY-remi /etc/pki/rpm-gpg/RPM-GPG-KEY-remi
 
 yum repolist
 yum install -y xenserver-core
