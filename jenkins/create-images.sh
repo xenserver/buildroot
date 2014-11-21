ffa404f8ce08c4b57275bbe4ec1e09946eb3ac91
Euan Harris <euan.harris@citrix.com>
2014-11-21 13:48:00 +0000

    jenkins: Add CentOS 7 Dockerfile
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- jenkins/create-images.sh
+++ jenkins/create-images.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker build --rm=true --force-rm=true --tag=jenkins-centos-6.5 centos-65
+docker build --rm=true --force-rm=true --tag=jenkins-centos-7.0 centos-70
 docker build --rm=true --force-rm=true --tag=jenkins-ubuntu-14.04 ubuntu-1404
 docker build --rm=true --force-rm=true --tag=jenkins-debian-jessie debian-jessie
 
