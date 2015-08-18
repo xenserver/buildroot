9e334026757eafe91f1197add49c03562c6da261
David Scott <dave@recoil.org>
2015-08-18 20:36:08 +0100

    Switch to CentOS 7, including the experimental Xen packages
    Signed-off-by: David Scott <dave@recoil.org>

--- scripts/rpm/install.sh
+++ scripts/rpm/install.sh
@@ -11,7 +11,7 @@ sed \
     -e "s,@XAPISRCBASEURL@,${XAPISRCBASEURL},g" \
     scripts/rpm/xapi.repo.in > scripts/rpm/xapi.repo
 
-for i in xapi Xen4CentOS ocaml-4.01 ; do
+for i in xapi CentOS-Xen ; do
     install -m 0644 scripts/rpm/$i.repo /etc/yum.repos.d/$i.repo
 done
 
